c4d4237407fd8bbebc765c2d4b54bca385860149
Nitendra Gautam <nitendra.gautam12@gmail.com>
2017-08-19 15:05:05 -0500

    Adding style to Header Component

--- Tutorial/react_redux_app/src/components/header.js
+++ Tutorial/react_redux_app/src/components/header.js
@@ -1,12 +1,60 @@
-import React from 'react';
+import React, {Component } from 'react';
 
+
+/*
+Creating a Header Class and binding to a React 
+This header components is a class based components 
+
+*/
+
+
+class Header  extends Component {
+    
+    render(){
+        
+    //Defining Styles for Header Component     
+  const headerStyles = {
+      header : {
+          background :'#03a9f4'
+      },
+      
+      logo :{
+          color:'#fff',
+          fontFamily:'Anton',
+          textAlign:'center'
+      }
+  }      
+        
+        
+        return (
+        <header style={headerStyles.header}>
+            <div style={headerStyles.logo}>Logo</div>
+            <input/>
+            </header>
+        )
+    }
+}
+
+
+
+
+export default Header;
+
+
+
+
+
+/*
 const Header = () => {
     
-    return <div></div>
+    return <div>Header for Page</div>
 }
 
 //React components  needs to be connected to main components
-//so that it is accessible from other components .To do that we use expo
+//so that it is accessible from other components .To do that we use export 
 
 //It means that Header is exported to all the application
-export default Header;
+
+*/
+
+
--- Tutorial/react_redux_app/src/index.js
+++ Tutorial/react_redux_app/src/index.js
@@ -2,17 +2,43 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 
-//Import Components Header
+//Import Header Components  inside Main Components
 //No need to give .js in components as react will recognize it 
+//To use Header Component we are importing Header component from header.js 
 
 import Header from './components/header'
+
 // React Component 
 const App = () => {
-    return (
+    //we can wrap  <h1> tag or different element  inside small bracket ()
+    //This returns Hello World with 
+    return (<div>
+            <Header/>
         <h1>Hello World</h1>
-        )
+        </div>
+            )
 }
 
 
 ReactDOM.render(<App/>,
                document.querySelector('#root'));
+
+
+/*
+This is the main component in which we can import other 
+components 
+
+Two main components in React :
+
+(a)Functional Component 
+(b) React Component 
+
+Functional component does not care about what happens to other components with the application 
+
+Class Component : It has the ability to know what happending with rest of the application
+
+React provides two methods for styling elements .They 
+are inline method and import method 
+
+
+*/
